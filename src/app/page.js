aada02f95ac91edae627dbbec8cfde55d9e53a3f
unknown <frncscgvdesarrollos@gmail.com>
2024-08-18 21:40:47 -0300

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex lg:flex-row flex-col overflow-x-hidden overflow-y-scroll lg:overflow-hidden w-[100vw] md:w-[100vw] lg:w-[80vw] mx-auto">
       <div className="flex flex-col bg-gray-700 lg:bg-gray-100 justify-start items-center py-4 px-4 border-l-4 border-red-900"> 
 
-        <div className="flex mt-[5rem] lg:mt-[2rem] mx-auto w-[25rem] h-[30rem] " >
+        <div className="flex mt-[1rem] lg:mt-[2rem] mx-auto w-[25rem] lg:h-[30rem] " >
           <Cubo  className="translate-x-[-20rem] translate-y-[-10rem] z-10"/>
           <img src="/webs.png" alt="https://www.flaticon.es/iconos-gratis/sitio-web" className="w-[20rem] h-[20rem] rounded-full mt-[2rem]  -translate-x-[5rem] " />
           <img src="/fotopersonal.png" alt="logo" className="w-[20rem] h-[20rem] rounded-full mt-[5rem] -translate-x-[20rem] translate-y-[2rem]" />
