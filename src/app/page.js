38a3fe957ac32e41907feedbe2e54934af4a3df4
unknown <frncscgvdesarrollos@gmail.com>
2024-08-19 10:52:10 -0300

    lunes

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex w-[100vw] mx-auto flex-col bg-gray-700 lg:bg-gray-100 justify-start items-center lg:py-4 lg:px-4 border-l-4 border-red-900 lg:w-[30vw] lg:h-[90vh]  lg:my-auto"> 
         <div className="flex  h-[40vh] mx-auto w-[100vw] justify-center " >
           <Cubo className=""  />
-          <div className="flex h-[40vh] w-[40vh] justify-center">
+          <div className="flex h-[40vh] w-[40vh] justify-center translate-x-[1rem]">
           <img src="/webs.png" alt="https://www.flaticon.es/iconos-gratis/sitio-web" className="h-[30vh] translate-y-[4rem] translate-x-[1rem] mx-auto rounded-full" />
           <img src="/fotopersonal.png" alt="logo" className="h-[30vh] rounded-full mx-auto translate-y-[9rem] translate-x-[-9rem]" />
           </div>
