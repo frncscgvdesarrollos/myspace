9dc3228c8365e87182bbc75519022aeb03e3102a
unknown <frncscgvdesarrollos@gmail.com>
2024-08-20 11:05:59 -0300

    martes SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata = {
   title: "Francisco Guerra - Full Stack Developer - Portfolio",
   description: "Desarrollador Web Full Stack",
+  keywords: "Francisco Guerra, Full Stack Developer, Desarrollador Web, Portfolio, JavaScript, React, Web Development, Portafolio, Argentino La pampa Santa Rosa",
+  author: "Francisco Guerra",
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
+  canonical: "https://frncscgvdesarrollos.vercel.app",
 };
 
 export default function RootLayout({ children }) {
@@ -11,13 +16,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="keywords" content="Francisco Guerra,
-         Full Stack Developer, Desarrollador Web, Portfolio,
-         JavaScript, React, Web Development, Portafolio, Argentino La pampa Santa Rosa" />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://frncscgvdesarrollos.vercel.app" />
+        <link rel="icon" href="../public/icons/favicon.ico" />
       </head>
-      <body className="w-[100%] md:w-[40rem] lg:w-[60rem] lg:mx-auto overflow-x-hidden">
+     <body className="w-[100%] md:w-[40rem] lg:w-[60rem] lg:mx-auto overflow-x-hidden">
             {children}
       </body>
     </html>
