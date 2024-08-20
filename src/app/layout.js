751491867c19c03122e8a481c6d6566788c550c5
unknown <frncscgvdesarrollos@gmail.com>
2024-08-20 10:36:35 -0300

    martes SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 
 export const metadata = {
-  title: "Francisco Guerra - Portfolio",
+  title: "Francisco Guerra - Full Stack Developer - Portfolio",
   description: "Desarrollador Web Full Stack",
 };
 
@@ -10,7 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="Francisco Guerra, Full Stack Developer, JavaScript, React, Web Development, Portfolio" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://frncscgvdesarrollos.vercel.app" />
       </head>
       <body className="w-[100%] md:w-[40rem] lg:w-[60rem] lg:mx-auto overflow-x-hidden">
             {children}
